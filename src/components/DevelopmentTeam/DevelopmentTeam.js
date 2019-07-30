d4d309146d65edd389bd1ec9257f98b66dc6c545
finnibyh <gnomdanik@gmail.com>
2019-07-31 01:11:42 +0300

    feat: add images to team page

--- src/components/DevelopmentTeam/DevelopmentTeam.js
+++ src/components/DevelopmentTeam/DevelopmentTeam.js
@@ -1,47 +1,63 @@
 import React from 'react'
 
 import styles from "./DevelopmentTeam.module.css"
+import github from "../../assets/images/github.svg"
+import gerasikImage from "../../assets/images/gerasik.jpg"
+import vldmrImage from "../../assets/images/vldmr1.jpg"
+import maliaxandrImage from "../../assets/images/maliaxandr.jpg"
+import velenaflowerImage from "../../assets/images/velenaflower.jpg"
+import finnibyhImage from "../../assets/images/finnibyh.jpg"
 
 const DevTeam = () => (
   <div className = {styles.team}>
     <div className = {styles.teamMember}>
-      <img src = "/" alt="MemberAvatar" className = {styles.memberAvatar}></img>
-      <h3>Member Name</h3>
+      <div className = {styles.memberAvatar}>
+        <img src = {gerasikImage} alt="MemberAvatar"></img>
+      </div>
+      <h3>Yauheni Herasimenka</h3>
       <span>
-        <img src="/" alt = 'github'></img>
-        <p className = {styles.memberGithub}>Member Github</p>
+        <img src={github} alt = 'github'></img>
+        <a href="https://github.com/gerasik" className = {styles.memberGithub}>gerasik</a>
       </span>
     </div>
     <div className = {styles.teamMember}>
-      <img src = "/" alt="MemberAvatar" className = {styles.memberAvatar}></img>
-      <h3>Member Name</h3>
+      <div className = {styles.memberAvatar}>
+        <img src = {vldmrImage} alt="MemberAvatar"></img>
+      </div>
+      <h3>Vladimir Malygin</h3>
       <span>
-        <img src="/" alt = 'github'></img>
-        <p className = {styles.memberGithub}>Member Github</p>
+        <img src={github} alt = 'github'></img>
+        <a href="https://github.com/vldmr1" className = {styles.memberGithub}>vldmr1</a>
       </span>
     </div>
     <div className = {styles.teamMember}>
-      <img src = "/" alt="MemberAvatar" className = {styles.memberAvatar}></img>
-      <h3>Member Name</h3>
+      <div className = {styles.memberAvatar}>
+        <img src = {maliaxandrImage} alt="MemberAvatar"></img>
+      </div>
+      <h3>Aliaksandr Mamchyts</h3>
       <span>
-        <img src="/" alt = 'github'></img>
-        <p className = {styles.memberGithub}>Member Github</p>
+        <img src={github} alt = 'github'></img> 
+        <a href="https://github.com/maliaxandr" className = {styles.memberGithub}>maliaxandr</a>
       </span>
     </div>
     <div className = {styles.teamMember}>
-      <img src = "/" alt="MemberAvatar" className = {styles.memberAvatar}></img>
-      <h3>Member Name</h3>
+      <div className = {styles.memberAvatar}>
+        <img src = {velenaflowerImage} alt="MemberAvatar"></img>
+      </div>
+      <h3>Alena Yanchanka</h3>
       <span>
-        <img src="/" alt = 'github'></img>
-        <p className = {styles.memberGithub}>Member Github</p>
+        <img src={github} alt = 'github'></img>
+        <a href="https://github.com/velenaflower" className = {styles.memberGithub}>velenaflower</a>
       </span>
     </div>
     <div className = {styles.teamMember}>
-      <img src = "/" alt="MemberAvatar" className = {styles.memberAvatar}></img>
-      <h3>Member Name</h3>
+      <div className = {styles.memberAvatar}>
+        <img src = {finnibyhImage} alt="MemberAvatar"></img>
+      </div>
+      <h3>Daniil Gaiduk</h3>
       <span>
-        <img src="/" alt = 'github'></img>
-        <p className = {styles.memberGithub}>Member Github</p>
+        <img src={github} alt = 'github'></img>
+        <a href="https://github.com/finnibyh" className = {styles.memberGithub}>finnibyh</a>
       </span>
     </div>
   </div>
