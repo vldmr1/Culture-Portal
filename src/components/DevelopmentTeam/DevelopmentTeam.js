fb50832bc4fcc7ada975a36ead35407d3e647e2e
finnibyh <gnomdanik@gmail.com>
2019-07-31 23:23:51 +0300

    feat: add creating components due 'map' method

--- src/components/DevelopmentTeam/DevelopmentTeam.js
+++ src/components/DevelopmentTeam/DevelopmentTeam.js
@@ -2,65 +2,18 @@ import React from 'react'
 
 import styles from "./DevelopmentTeam.module.css"
 import github from "../../assets/images/github.svg"
-import gerasikImage from "../../assets/images/gerasik.jpg"
-import vldmrImage from "../../assets/images/vldmr1.jpg"
-import maliaxandrImage from "../../assets/images/maliaxandr.jpg"
-import velenaflowerImage from "../../assets/images/velenaflower.jpg"
-import finnibyhImage from "../../assets/images/finnibyh.jpg"
 
-const DevTeam = () => (
-  <div className = {styles.team}>
+const DevTeam = (props) => (
     <div className = {styles.teamMember}>
       <div className = {styles.memberAvatar}>
-        <img src = {gerasikImage} alt="MemberAvatar"></img>
+        <img src = {`../../assets/images/${props.image}.jpg`} alt="MemberAvatar"></img>
       </div>
-      <h3>Yauheni Herasimenka</h3>
+      <h3>{props.name}</h3>
       <span>
         <img src={github} alt = 'github'></img>
-        <a href="https://github.com/gerasik" className = {styles.memberGithub}>gerasik</a>
+        <a href={`https://github.com/${props.github}`} className = {styles.memberGithub}>{props.github}</a>
       </span>
     </div>
-    <div className = {styles.teamMember}>
-      <div className = {styles.memberAvatar}>
-        <img src = {vldmrImage} alt="MemberAvatar"></img>
-      </div>
-      <h3>Vladimir Malygin</h3>
-      <span>
-        <img src={github} alt = 'github'></img>
-        <a href="https://github.com/vldmr1" className = {styles.memberGithub}>vldmr1</a>
-      </span>
-    </div>
-    <div className = {styles.teamMember}>
-      <div className = {styles.memberAvatar}>
-        <img src = {maliaxandrImage} alt="MemberAvatar"></img>
-      </div>
-      <h3>Aliaksandr Mamchyts</h3>
-      <span>
-        <img src={github} alt = 'github'></img> 
-        <a href="https://github.com/maliaxandr" className = {styles.memberGithub}>maliaxandr</a>
-      </span>
-    </div>
-    <div className = {styles.teamMember}>
-      <div className = {styles.memberAvatar}>
-        <img src = {velenaflowerImage} alt="MemberAvatar"></img>
-      </div>
-      <h3>Alena Yanchanka</h3>
-      <span>
-        <img src={github} alt = 'github'></img>
-        <a href="https://github.com/velenaflower" className = {styles.memberGithub}>velenaflower</a>
-      </span>
-    </div>
-    <div className = {styles.teamMember}>
-      <div className = {styles.memberAvatar}>
-        <img src = {finnibyhImage} alt="MemberAvatar"></img>
-      </div>
-      <h3>Daniil Gaiduk</h3>
-      <span>
-        <img src={github} alt = 'github'></img>
-        <a href="https://github.com/finnibyh" className = {styles.memberGithub}>finnibyh</a>
-      </span>
-    </div>
-  </div>
   
 );
 
