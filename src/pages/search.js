420cc55c66268b9fdaeb64eccac6b3a6633aeb3e
Gerasik <gerasik1992@gmail.com>
2019-07-31 22:48:18 +0300

    feat: create search page

---
+++ src/pages/search.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Search from "../components/Search/Search"
+
+const autorsList = [
+    "Пимен Емельянович Панченко",
+    "Рыгор (Григорий) Иванович Бородулин",
+    "Якуб Колас",
+    "Янка Купала",
+    "Максим Адамович Богданович"
+];
+
+const SearchPage = () => (
+  <Layout>
+    <SEO title="Search" />
+    <Search autorsList={autorsList}/>
+  </Layout>
+)
+
+export default SearchPage
