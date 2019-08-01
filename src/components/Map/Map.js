40bfada018ce1d6138d8f1b8bfc0bc6f99cadd00
Elena Yanchenko <veleno.ajt@gmail.com>
2019-08-01 15:29:42 +0300

    refactor: change Map component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
-import { Map, TileLayer, Marker } from 'react-leaflet';
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import styles from "./Map.module.css";
 
-export default class LeafletMap extends Component {
-  state = {
-    lat: 53.9061114,
-    lng: 27.5651102,
-    zoom: 15,
-  };
+const Map = (props) => {
+  const url = "https://www.google.com/maps/d/embed?mid=" + props.id;
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <iframe className = {styles.sizeMap} title="poet's map" src={url}>
+          </iframe>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-  render() {
-    const position = [this.state.lat, this.state.lng];
-    return (
-      <Map center={position} zoom={this.state.zoom} className = {styles.sizeMap}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-        </Marker>
-      </Map>
-    )
-  }
-}
+export default Map;
