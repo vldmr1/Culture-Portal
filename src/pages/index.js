621704696a524a843a337d0aee0bb3228b6e6f67
Vladimir Malygin <vldmr.one@gmail.com>
2019-08-02 14:11:31 +0300

    feat: add locale selection

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,22 +2,21 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { FormattedMessage, injectIntl } from "gatsby-plugin-intl"
 
-const IndexPage = () => (
+const IndexPage = ({ intl }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>This page is about Belorussian poets</h1>
-    <p>Lorem Ipsum is simply dummy text of the printing
-      and typesetting industry. Lorem Ipsum has been the industry's
-      standard dummy text ever since the 1500s, when an unknown printer
-      took a galley of type and scrambled it to make a type specimen book.
-      It has survived not only five centuries, but also the leap into electronic
-      typesetting, remaining essentially unchanged. It was popularised in the 1960s
-      with the release of Letraset sheets containing Lorem Ipsum passages, and more
-      recently with desktop publishing software like Aldus PageMaker including versions
-      of Lorem Ipsum.</p>
-    <h2>Author of the day</h2>
+    <h1>
+      <FormattedMessage id="main_page_header" />
+    </h1>
+    <p>
+      <FormattedMessage id="main_page_introduction" />
+    </p>
+    <h2>
+      <FormattedMessage id="author_of_the_day_title" />
+    </h2>
   </Layout>
 )
 
-export default IndexPage
+export default injectIntl(IndexPage)
