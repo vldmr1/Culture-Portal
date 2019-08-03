19ee02d409f4c7e937c8e7761aedf578d3080ec6
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2019-08-03 17:47:31 +0300

    feat: add video component in authorOfTheDay component

--- src/components/AuthorOfTheDay/AuthorOfTheDay.js
+++ src/components/AuthorOfTheDay/AuthorOfTheDay.js
@@ -12,6 +12,9 @@ const AuthorOfTheDay = () => {
             date='1220'
             description='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
         />
+        <Video
+            id="HxFeWGZspJQ"
+        />
     </>
     )
 }
